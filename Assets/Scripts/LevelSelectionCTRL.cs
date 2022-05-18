6a71f247be668d48610cbe57e9a3a1ad69d64be2
Tanvi <66575649+programmer-pixel@users.noreply.github.com>
2022-05-17 17:16:30 -0700

    upload source code

---
+++ Assets/Scripts/LevelSelectionCTRL.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelectionCTRL : MonoBehaviour{
+    public void LoadScene(string sceneName){
+        SceneManager.LoadScene(sceneName);
+    }
+}
